e442e008cc632f013e4068acb4cf5c02383a0047
Tanay <iamtanaylodh@gmail.com>
2020-09-01 07:38:18 +0530

    Added PiP function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Testing');
+const videoElement =  document.getElementById('video');
+const btn = document.getElementById('button');
+
+async function getMediaStream(){
+    // Getting media source
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    // Play video on load
+    videoElement.onloadedmetadata = () => {
+        videoElement.play();
+    }
+}
+
+// Button triggers PiP on click
+btn.addEventListener('click', async () => {
+    // Disable button on click
+    btn.disabled = true;
+    // Trigger PiP
+    await videoElement.requestPictureInPicture();
+    // Reset button state
+    btn.disabled = false;
+})
+
+getMediaStream();
